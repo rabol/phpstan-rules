c067ae191d025445059fe07da408ca5b6b6d17b3
rabol <steen@rabol.com>
2023-10-02 19:59:15 +0200

    update

--- src/Rules/NoDuplicateMemberName/NoDuplicateMemberNameRule.php
+++ src/Rules/NoDuplicateMemberName/NoDuplicateMemberNameRule.php
@@ -9,40 +9,35 @@ use PhpParser\Node;
 use PHPStan\Analyser\Scope;
 use PHPStan\Rules\Rule;
 use PHPStan\ShouldNotHappenException;
+use PhpParser\Node\Stmt\Class_;
 
-class NoDuplicateMemberNameRule implements Rule
+class NoPropertyAndMethodWithSameNameRule implements Rule
 {
     public function getNodeType(): string
     {
-        return Node\Stmt\Class_::class;
+        return Class_::class;
     }
 
     public function processNode(Node $node, Scope $scope): array
     {
-        if (!$node instanceof Node\Stmt\Class_) {
-            throw new ShouldNotHappenException();
-        }
+        $errors = [];
 
-        $memberNames = [];
-
+        /** @var Class_ $node */
+        $propertyNames = [];
         foreach ($node->stmts as $stmt) {
             if ($stmt instanceof Node\Stmt\Property) {
                 foreach ($stmt->props as $prop) {
-                    $memberNames[$prop->name->toString()] = 'property';
+                    $propertyNames[] = $prop->name->toString();
                 }
-            }
-
-            if ($stmt instanceof Node\Stmt\ClassMethod) {
-                $memberNames[$stmt->name->toString()] = 'method';
             }
         }
 
-        $errors = [];
-        $countValues = array_count_values($memberNames);
-
-        foreach ($countValues as $name => $count) {
-            if ($count > 1) {
-                $errors[] = sprintf('Class %s has a %s and method with the name "%s".', $scope->getClassReflection()->getName(), $memberNames[$name], $name);
+        foreach ($node->stmts as $stmt) {
+            if ($stmt instanceof Node\Stmt\ClassMethod) {
+                $methodName = $stmt->name->toString();
+                if (in_array($methodName, $propertyNames, true)) {
+                    $errors[] = sprintf('Property and method with the name "%s" found in class.', $methodName);
+                }
             }
         }
 
